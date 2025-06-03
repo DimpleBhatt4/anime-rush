7302a30aaf24cda2ba60f5a84620b5fdee133b43
DimpleBhatt4 <dimplebhatt002@gmail.com>
2025-06-03 15:06:48 +0530

    Adjust the search text, make it in a single line

--- src/app/components/searchbar/SearchBar.jsx
+++ src/app/components/searchbar/SearchBar.jsx
@@ -9,7 +9,7 @@ const SearchBar = () => {
     <>
       <button
        
-        className='bg-black border border-white w-[50vw] md:w-[30vw]  rounded-lg  px-4 py-2 md:px-2 md:py-2 text-left text-xs md:text-md'
+        className='bg-black border border-white  md:w-[30vw]  rounded-lg  px-2  py-2 md:px-4 text-center md:text-left text-xs md:text-md'
         onClick={() => setSearchClicked(!searchClicked)}>
         Search your favorite anime here..
       </button>
